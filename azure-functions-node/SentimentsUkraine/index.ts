6ea161b8ea54f747f68378bc7ecf303e842d9490
shenghaoc <shenghaoc@outlook.com>
2022-05-01 23:01:30 +0800

    Expand zip file

---
+++ azure-functions-node/SentimentsUkraine/index.ts
@@ -0,0 +1,40 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as db from "../lib/azure-cs4225-output-ukraine";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    let response = null;
+
+    // create 1 db connection for all functions
+    await db.init();
+
+    switch (req.method) {
+      case "GET":
+          // allows empty query to return all items
+          const dbQuery =
+            req?.query?.dbQuery || (req?.body && req?.body?.dbQuery);
+          response = {
+            documentResponse: await db.findItems(dbQuery),
+          };
+        break;
+      default:
+        throw Error(`${req.method} not allowed`)
+    }
+
+    context.res = {
+      body: response,
+    };
+  } catch (err) {
+    context.log(`*** Error throw: ${JSON.stringify(err)}`);
+
+    context.res = {
+      status: 500,
+      body: err,
+    };
+  }
+};
+
+export default httpTrigger;
---
+++ azure-functions-node/lib/azure-cs4225-output-overall.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb'
+
+let db=null;
+
+const client = new MongoClient(process.env["CosmosDbConnectionString"])
+
+export const init = async () => {
+  if(!db) {
+    await client.connect()
+    db = client.db('sentanaly')
+  }
+};
+export const findItems = async (query = {}) => {
+  return db.collection("sentiments_overall").find({}).toArray();
+};
